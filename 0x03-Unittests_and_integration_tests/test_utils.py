7837b36f85500831cfbb8cf6a10509aa80077451
root <root@DESKTOP-F8D6VBB.localdomain>
2025-05-30 19:48:10 +0100

    remove decorator names

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 A module for testing the utils module.
 
 """
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
 
 import unittest
 from utils import access_nested_map
@@ -12,12 +12,12 @@ from typing import (Dict, Tuple, Any)
 class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
     @parameterized.expand([
-        ("no_nest", {"a": 1}, ("a",), 1),
-        ("one_nest", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("two_nest", {"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
    ])
     
-    def test_access_nested_map(self, name: str, nested_map: Dict, path: Tuple[str], expected: Any) -> None:
-        """Test `access_nested_map` output."""
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str], expected: Any) -> None:
+        """Test `access_nested_map`'s output."""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
