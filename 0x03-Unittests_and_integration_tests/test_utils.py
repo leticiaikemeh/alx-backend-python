26df2790ff5ea3e02f18de33e493f8f093faba6e
root <root@DESKTOP-F8D6VBB.localdomain>
2025-05-30 19:28:06 +0100

    add annotations

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
+"""
+A module for testing the utils module.
+
+"""
 from parameterized import parameterized, parameterized_class
 
 import unittest
 from utils import access_nested_map
-from typing import (
-    Mapping,
-    Sequence,
-    Any,
-    Dict,
-    Callable,
-)
+from typing import (Dict, Tuple, Any)
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ("no_nest", {"a": 1}, ("a",), 1),
         ("one_nest", {"a": {"b": 2}}, ("a",), {"b": 2}),
         ("two_nest", {"a": {"b": 2}}, ("a", "b"), 2)
    ])
-    def test_access_nested_map(self, name: str, nested_map: Mapping, path: Sequence, expected: Any):
-        """Test valid nested map cases"""
+    
+    def test_access_nested_map(self, name: str, nested_map: Dict, path: Tuple[str], expected: Any) -> None:
+        """Test `access_nested_map` output."""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
-
