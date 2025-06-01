0533a03021497738f7ff100c6cb79f83ee7ea9ec
root <root@DESKTOP-F8D6VBB.localdomain>
2025-06-01 18:52:05 +0100

    pycodestyle checked

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,8 +20,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(
             self, nested_map: Dict, path: Tuple[str],
-            expected: Any
-            ) -> None:
+            expected: Any) -> None:
         """Test `access_nested_map`'s output with input cases."""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
@@ -66,4 +65,18 @@ class TestMemoize(unittest.TestCase):
         original method only once.
         """
         class TestClass:
-            def a_method(self)_
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
+            test_obj = TestClass()
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
